804ae22c7fd97a8a7f44516bdbb0cce6d19d1bad
Augustyn <augustynzentner@gmail.com>
2021-05-07 14:57:55 +0200

    querying actual pocs endpoint

--- src/app/core/models/poc-filters.ts
+++ src/app/core/models/poc-filters.ts
@@ -1,6 +1,8 @@
 import { SortDirection } from '@angular/material/sort';
 import { DEFAULT_PAGE_SIZE } from '../utils/constants';
 
+const FILTER_PARAM_PREFIX = 'filterColumn';
+
 export class PocFilters {
     search?: string = undefined;
     sortColumn?: string = undefined;
@@ -13,3 +15,15 @@ export class PocFilters {
 export interface FilterColumn {
     [column: string]: string;
 }
+
+export const flattenFilters = (filters: PocFilters) => {
+    const { filterColumns, ...other } = filters;
+    if (!filterColumns) { return filters; }
+
+    const flatFilters = Object.keys(filterColumns).reduce((acc, key) => {
+        const newKey = `${FILTER_PARAM_PREFIX}${key}`;
+        acc[newKey] = filters.filterColumns[key];
+        return acc;
+    }, {});
+    return { ...other, ...flatFilters };
+};
--- src/app/core/services/pocs.service.ts
+++ src/app/core/services/pocs.service.ts
@@ -1,53 +1,31 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { MOCK_DATA } from '../mocks/pocs.mock';
 import { IListResult } from '../models/interfaces/poc-list-result.interface';
 import { IPoc } from '../models/interfaces/poc.interface';
-import { PocFilters } from '../models/poc-filters';
+import { flattenFilters, PocFilters } from '../models/poc-filters';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PocsService {
 
-  pocsList = MOCK_DATA;
+  pocListUrl = `${environment.pocManagerApi}${environment.pocList}`;
 
   constructor(private http: HttpClient) { }
 
   loadPocs(filters: PocFilters): Observable<IListResult<IPoc>> {
-    let data = this.pocsList;
-
-    if (filters.search?.trim()) {
-      data = data.filter(r => JSON.stringify(r).includes(filters.search));
-    }
-
-    if (filters.filterColumns?.status?.length > 0) {
-      data = data.filter(r => filters.filterColumns.status.includes(r.status));
-    }
-
-    if (filters.sortColumn) {
-      data = data.slice().sort((a: IPoc, b: IPoc) => {
-        if (typeof a[filters.sortColumn] === 'number') { return a[filters.sortColumn] - b[filters.sortColumn]; }
-        else { return ('' + a[filters.sortColumn]).localeCompare(b[filters.sortColumn]); }
-      });
-      data = filters.sortOrder === 'asc' ? data : data.reverse();
-    }
-
-    return of({
-      total: data.length,
-      pocs: data.slice(filters.pageIndex * filters.pageSize, filters.pageIndex * filters.pageSize + filters.pageSize)
-    } as IListResult<IPoc>).pipe(delay(1000));
-
-    // TODO: Implement actual processing of datafetching
+    return this.http.get<IListResult<IPoc>>(this.pocListUrl, { params: flattenFilters(filters) as any });
   }
 
   deletePocs(pocs: IPoc[]) {
-    const count = this.pocsList.length;
-    const pocIds = pocs.map(p => p.id);
-    this.pocsList = this.pocsList.filter(p => !pocIds.includes(p.id));
-    return of(count - this.pocsList.length);
+    // const count = this.pocsList.length;
+    // const pocIds = pocs.map(p => p.id);
+    // this.pocsList = this.pocsList.filter(p => !pocIds.includes(p.id));
+    return of([]);
   }
 
 }
