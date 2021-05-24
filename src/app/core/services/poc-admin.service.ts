67ecdd60cec36dcabcf573ed930d2af5ecd8955e
dieAte <beate.fiss@indarium.de>
2021-05-24 15:45:19 +0200

    activated PoCAdmin endpoints - not tested!

--- src/app/core/services/poc-admin.service.ts
+++ src/app/core/services/poc-admin.service.ts
@@ -6,6 +6,7 @@ import { environment } from 'src/environments/environment';
 import { ADMINS_MOCK, ADMIN_STATE_MOCK } from '../mocks/admins.mock';
 import { Filters, flattenFilters } from '../models/filters';
 import { IListResult } from '../models/interfaces/list-result.interface';
+import { IPocAdminState } from '../models/interfaces/poc-admin-state.interface';
 import { IPocAdmin } from '../models/interfaces/poc-admin.interface';
 import { IWebIdentConfirmation } from '../models/interfaces/web-ident-confirmation.interface';
 import { ErrorHandlerService } from './error-handler.service';
@@ -33,24 +34,24 @@ export class PocAdminService {
   }
 
   getAdmins(filters: Filters) {
-    return of(ADMINS_MOCK).pipe(delay(1000));
+//    return of(ADMINS_MOCK).pipe(delay(1000));
 
-    // return this.http.get<IListResult<IPocAdmin>>(this.adminsUrl, { params: flattenFilters(filters) as any });
+    return this.http.get<IListResult<IPocAdmin>>(this.adminsUrl, { params: flattenFilters(filters) as any });
   }
 
 
   getAdminState(adminId: string) {
-    return of(ADMIN_STATE_MOCK).pipe(delay(500));
+//    return of(ADMIN_STATE_MOCK).pipe(delay(500));
 
-    // const url = `${this.adminStatusUrl}/${adminId}`;
-    // return this.http.get<IPocAdminState>(url);
+    const url = `${this.adminStatusUrl}/${adminId}`;
+    return this.http.get<IPocAdminState>(url);
   }
 
   getInitialIdentId(adminId: string) {
-    return of('74575b09-6699-4f09-b1b2-dc8e456e0c97').pipe(delay(500));
+//    return of('74575b09-6699-4f09-b1b2-dc8e456e0c97').pipe(delay(500));
 
-    // const url = `${this.adminsIdentUrl}/${adminId}`;
-    // return this.http.get<string>(url);
+    const url = `${this.adminsIdentUrl}/${adminId}`;
+    return this.http.get<string>(url);
   }
 
   postWebIdentId(confirm: IWebIdentConfirmation) {
