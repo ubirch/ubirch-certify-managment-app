bbb4ff39d3cf56327df334d900f78eac40a7e67d
Augustyn <augustynzentner@gmail.com>
2021-05-31 15:07:07 +0200

    adjusted required fields for poc status

--- src/app/core/models/interfaces/poc-state.interface.ts
+++ src/app/core/models/interfaces/poc-state.interface.ts
@@ -1,27 +1,27 @@
 export interface IPocState {
-    certifyRoleCreated?: boolean;
-    certifyGroupCreated?: boolean;
-    certifyGroupRoleAssigned?: boolean;
+    certifyRoleCreated: boolean;
+    certifyGroupCreated: boolean;
+    certifyGroupRoleAssigned: boolean;
     adminGroupCreated?: boolean;
     adminRoleAssigned?: boolean;
     employeeGroupCreated?: boolean;
     employeeRoleAssigned?: boolean;
-    deviceRoleCreated?: boolean;
-    deviceGroupCreated?: boolean;
-    deviceGroupRoleAssigned?: boolean;
-    deviceCreated?: boolean;
-    assignedDataSchemaGroup?: boolean;
-    assignedTrustedPocGroup?: boolean;
-    assignedDeviceGroup?: boolean;
-    clientCertRequired?: boolean;
+    deviceRoleCreated: boolean;
+    deviceGroupCreated: boolean;
+    deviceGroupRoleAssigned: boolean;
+    deviceCreated: boolean;
+    assignedDataSchemaGroup: boolean;
+    assignedTrustedPocGroup: boolean;
+    assignedDeviceGroup: boolean;
+    clientCertRequired: boolean;
     orgUnitCertCreated?: boolean;
     clientCertCreated?: boolean;
     clientCertProvided?: boolean;
-    logoRequired?: boolean;
+    logoRequired: boolean;
     logoReceived?: boolean;
     logoStored?: boolean;
-    goClientProvided?: boolean;
-    certifyApiProvided?: boolean;
+    goClientProvided: boolean;
+    certifyApiProvided: boolean;
 
     errorMessage: string;
 
