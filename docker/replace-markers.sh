9d6f3bfe9f74dce6dd0d72b3e56bbd314ad2da10
Jonas <jonas@jointech.org>
2021-06-25 02:00:14 +0200

    Switch to bitnami/nginx
    The reason for that is, that there is, that the bitnami nginx
    is guaranteed to be functional when starting with a nonzero uid.
    This comes with the side effect, that the container no longer
    exposes port 80, but is now exposing port 8081

--- docker/replace-markers.sh
+++ docker/replace-markers.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # replace the markers in any javascript file.
-find /usr/share/nginx/html -type f -name '*.js' -exec \
+find /template_app -type f -name '*.js' -exec \
     sed -i \
         -e "s%@@KEYCLOAK_REALM@@%${KEYCLOAK_REALM}%" \
         -e "s%@@KEYCLOAK_URL@@%${KEYCLOAK_URL}%" \
         -e "s%@@KEYCLOAK_CLIENT_ID@@%${KEYCLOAK_CLIENT_ID}%" \
         -e "s%@@POC_MANAGER_API@@%${POC_MANAGER_API}%" \
         {} \;
+
+cp -a /template_app/* /www
