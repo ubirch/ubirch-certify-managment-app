3370e3daf8795a63d727a24fcc1e3d01db80daf5
Augustyn <augustynzentner@gmail.com>
2021-08-03 12:46:20 +0200

    UNG-1937: fixed userInRole directive

--- src/app/shared/directives/user-in-role.directive.spec.ts
+++ src/app/shared/directives/user-in-role.directive.spec.ts
@@ -1,5 +1,3 @@
-import { UserInRoleDirective } from './user-in-role.directive';
-
 describe('IfInRoleDirective', () => {
   it('should create an instance', () => {
     // const directive = new UserInRoleDirective();
