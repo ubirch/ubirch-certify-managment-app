8355f21c0feeac92d32df0bdbee2862da4b826ec
Panos Platis <panos.platis@ubirch.com>
2022-08-01 10:25:11 +0200

    provide revocation pdf upload -> extract QR Code automatically

--- src/app/shared/components/file-upload/file-upload.component.ts
+++ src/app/shared/components/file-upload/file-upload.component.ts
@@ -1,98 +1,127 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import {
+    AfterViewInit,
+    Component,
+    ElementRef,
+    EventEmitter,
+    Input,
+    Output,
+    ViewChild,
+} from '@angular/core';
 import { fromEvent, merge, Observable } from 'rxjs';
 import { filter, tap } from 'rxjs/operators';
 import { UploadState } from 'src/app/core/models/enums/upload-state.enum';
 import { IUploadStatus } from 'src/app/core/models/interfaces/upload-status.interface';
 
 @Component({
-  selector: 'app-file-upload',
-  templateUrl: './file-upload.component.html',
-  styleUrls: ['./file-upload.component.scss'],
+    selector: 'app-file-upload',
+    templateUrl: './file-upload.component.html',
+    styleUrls: ['./file-upload.component.scss'],
 })
 export class FileUploadComponent implements AfterViewInit {
-  file: File;
-  accept = '.csv';
-  draggedOver = false;
-  uploadStates = UploadState;
+    file: any;
+    // accept = accept;
+    _accept: string;
+    draggedOver = false;
+    uploadStates = UploadState;
 
-  @ViewChild('input') input: ElementRef;
-  @ViewChild('drop') drop: ElementRef;
+    @ViewChild('input') input: ElementRef;
+    @ViewChild('drop') drop: ElementRef;
 
-  // tslint:disable-next-line:variable-name
-  private _progress: IUploadStatus;
-  get progress() { return this._progress; }
-  @Input() set progress(value: IUploadStatus) {
-    this._progress = value;
-    if (value?.state === UploadState.done) { this.resetUpload(); }
-  }
+    private _progress: IUploadStatus;
+    get progress() {
+        return this._progress;
+    }
+    @Input() set progress(value: IUploadStatus) {
+        this._progress = value;
+        if (value?.state === UploadState.done) {
+            this.resetUpload();
+        }
+    }
 
-  @Output() fileSelected = new EventEmitter<File>();
+    @Input() set accept(value: string) {
+        this._accept = value;
+    }
 
-  fileDropped$: Observable<any>;
+    @Output() fileSelected = new EventEmitter<File>();
 
-  get disabled(): boolean { return [UploadState.inPorgress, this.uploadStates.pending].includes(this.progress?.state); }
-  get fileSize(): number { return this.file?.size; }
-  get fileName(): string { return this.file?.name ?? 'import.filePlaceholder'; }
+    fileDropped$: Observable<any>;
 
-  constructor() { }
+    get disabled(): boolean {
+        return [UploadState.inPorgress, this.uploadStates.pending].includes(
+            this.progress?.state
+        );
+    }
+    get fileSize(): number {
+        if (this.file?.size === 20) {
+            return this.file?.files[0].size;
+        }
+        return this.file?.size;
+    }
+    get fileName(): string {
+        if (this.file?.name === '') {
+            return this.file?.files[0].name;
+        }
+        return this.file?.name ?? 'import.filePlaceholder';
+    }
 
-  ngAfterViewInit(): void {
-    merge(
-      fromEvent(this.drop.nativeElement, 'dragover').pipe(filter(v => !this.disabled)),
-      fromEvent(this.drop.nativeElement, 'dragenter').pipe(
-        filter(v => !this.disabled),
-        tap(() => this.draggedOver = true)
-      ),
-      fromEvent(this.drop.nativeElement, 'dragleave').pipe(
-        filter(v => !this.disabled),
-        tap(() => this.draggedOver = false)
-      ),
-      fromEvent(this.drop.nativeElement, 'drop').pipe(
-        filter(v => !this.disabled),
-        tap((event: DragEvent) => {
-          const filesList = event.dataTransfer?.files;
-          for (let i = 0; i < filesList?.length ?? 0; i++) {
-            const file = filesList.item(i);
-            if (file.type === 'text/csv') {
-              this.file = file;
-              this.fileChanaged();
-              this.draggedOver = false;
-              return;
-            }
-          }
-        })
-      ),
-    ).subscribe(
-      (event: DragEvent) => {
+    constructor() {}
+
+    ngAfterViewInit(): void {
+        merge(
+            fromEvent(this.drop.nativeElement, 'dragover').pipe(
+                filter((v) => !this.disabled)
+            ),
+            fromEvent(this.drop.nativeElement, 'dragenter').pipe(
+                filter((v) => !this.disabled),
+                tap(() => (this.draggedOver = true))
+            ),
+            fromEvent(this.drop.nativeElement, 'dragleave').pipe(
+                filter((v) => !this.disabled),
+                tap(() => (this.draggedOver = false))
+            ),
+            fromEvent(this.drop.nativeElement, 'drop').pipe(
+                filter((v) => !this.disabled),
+                tap((event: DragEvent) => {
+                    const filesList = event.dataTransfer?.files;
+                    for (let i = 0; i < filesList?.length ?? 0; i++) {
+                        const file = filesList.item(i);
+                        if (file.type === 'text/csv' || 'application/pdf') {
+                            this.file = file;
+                            this.fileChanaged();
+                            this.draggedOver = false;
+                            return;
+                        }
+                    }
+                })
+            )
+        ).subscribe((event: DragEvent) => {
+            event.preventDefault();
+            event.stopPropagation();
+        });
+    }
+
+    triggerrFileSelect() {
+        this.input.nativeElement.click();
+    }
+
+    selected(event) {
+        this._accept === '.csv'
+            ? (this.file = event?.target?.files?.[0])
+            : (this.file = event?.srcElement);
+        this.fileChanaged();
+    }
+
+    fileDropped(event) {
         event.preventDefault();
-        event.stopPropagation();
-      });
+    }
 
-  }
+    private fileChanaged() {
+        this.fileSelected.emit(this.file);
+    }
 
-  triggerrFileSelect() {
-    this.input.nativeElement.click();
-  }
-
-  selected(event) {
-    this.file = event?.target?.files?.[0];
-    this.fileChanaged();
-  }
-
-  fileDropped(event) {
-    event.preventDefault();
-  }
-
-  private fileChanaged() {
-    this.fileSelected.emit(this.file);
-  }
-
-  private resetUpload() {
-    this.file = undefined;
-    this.fileChanaged();
-    this.input.nativeElement.value = '';
-  }
-
-
-
+    private resetUpload() {
+        this.file = undefined;
+        this.fileChanaged();
+        this.input.nativeElement.value = '';
+    }
 }
