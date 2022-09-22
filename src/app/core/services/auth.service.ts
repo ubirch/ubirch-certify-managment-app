dfda88d074bb76e8a9ef78902402e7f26a537d68
Panos Platis <panos.platis@ubirch.com>
2022-09-22 12:18:01 +0300

    CPI-861 Assign both requester and authorizer roles to same user
    Use menu to switch between two pages if user has both roles.
    Menu is not visible when user has only one role.

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,38 +1,67 @@
 import { Injectable } from '@angular/core';
 import { KeycloakService } from 'keycloak-angular';
-import { POC_ADMIN_ROLE, REVOCATION_AUTHORIZER_ROLE, REVOCATION_REQUESTER_ROLE, ROLE, TENANT_ADMIN_ROLE } from '../models/roles';
+import {
+    POC_ADMIN_ROLE,
+    REVOCATION_AUTHORIZER_ROLE,
+    REVOCATION_REQUESTER_ROLE,
+    ROLE,
+    TENANT_ADMIN_ROLE,
+} from '../models/roles';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class AuthService {
+    constructor(private keycloak: KeycloakService) {}
 
-  constructor(
-    private keycloak: KeycloakService
-  ) { }
+    hasAnyRole(userRoles: ROLE | ROLE[]) {
+        const roles = this.keycloak.getUserRoles();
+        return Array.isArray(userRoles)
+            ? userRoles.some((r) => roles.includes(r))
+            : roles.includes(userRoles);
+    }
 
-  hasAnyRole(userRoles: ROLE | ROLE[]) {
-    const roles = this.keycloak.getUserRoles();
-    return Array.isArray(userRoles) ? userRoles.some(r => roles.includes(r)) : roles.includes(userRoles);
-  }
+    isTenantAdmin() {
+        const roles = this.keycloak.getUserRoles();
+        return roles?.includes(TENANT_ADMIN_ROLE);
+    }
 
-  isTenantAdmin() {
-    const roles = this.keycloak.getUserRoles();
-    return roles?.includes(TENANT_ADMIN_ROLE);
-  }
+    isPocAdmin() {
+        const roles = this.keycloak.getUserRoles();
+        return roles?.includes(POC_ADMIN_ROLE);
+    }
 
-  isPocAdmin() {
-    const roles = this.keycloak.getUserRoles();
-    return roles?.includes(POC_ADMIN_ROLE);
-  }
+    isRevocationRequester() {
+        const roles = this.keycloak.getUserRoles();
+        console.log(roles);
+        
+        return (
+            roles?.includes(REVOCATION_REQUESTER_ROLE) &&
+            !roles?.includes(REVOCATION_AUTHORIZER_ROLE)
+        );
+    }
 
-  isRevocationRequester() {
-    const roles = this.keycloak.getUserRoles();
-    return roles?.includes(REVOCATION_REQUESTER_ROLE);
-  }
-  
-  isRevocationAuthorizer() {
-    const roles = this.keycloak.getUserRoles();
-    return roles?.includes(REVOCATION_AUTHORIZER_ROLE);
-  }
+    isRevocationAuthorizer() {
+        const roles = this.keycloak.getUserRoles();
+        return (
+            roles?.includes(REVOCATION_AUTHORIZER_ROLE) &&
+            !roles?.includes(REVOCATION_REQUESTER_ROLE)
+        );
+    }
+
+    hasBothRoles() {
+        const roles = this.keycloak.getUserRoles();
+        return (
+            roles?.includes(REVOCATION_AUTHORIZER_ROLE) &&
+            roles?.includes(REVOCATION_REQUESTER_ROLE)
+        );
+    }
+
+    isRevocationApp() {
+      const roles = this.keycloak.getUserRoles();
+        return (
+            roles?.includes(REVOCATION_AUTHORIZER_ROLE) ||
+            roles?.includes(REVOCATION_REQUESTER_ROLE)
+        );
+    }
 }
